9d9cdfee369c57945669abce1b892e6c80d78f12
DocCreeps <dorian1998dino@hotmail.com>
2023-06-30 13:57:13 +0200

    🔨 add migration and model

---
+++ app/Models/Membres.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Membres extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'pseudo',
+        'grade',
+        'entryDate',
+        'picture',
+        'isActif',
+    ];
+}
